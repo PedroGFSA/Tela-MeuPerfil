602c3079152bd7ff10e12cb81d072c721c44819b
PedroGFSA <pedrogfsa2002@gmail.com>
2023-09-01 00:56:53 -0300

    Barra de pesquisa e aviso

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import Perfil from './Perfil'
 const App = () => {
 
+  const [searchBar, setSearchBar] = React.useState('')
+
+
   return (
-    <div>App</div>
+    <div>
+      <form className='containerHeader'>
+        <input type="text" placeholder='      Buscar capacitações, profissionais...'
+        value={searchBar}
+        onChange={(event) => setSearchBar(event.target.value)}/>
+      </form>
+      <div className="container-aviso">
+        <img src="src\assets\atencao.png" alt="aviso" />
+        <p
+        style={{textAlign: "left"}}><b>Completar perfil:</b> preencha suas informações para encontrar pacientes. 
+        </p>
+      </div>
+      <Perfil/>
+      
+    
+    </div>
   )
 }
 
---
+++ src/Perfil.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Perfil = () => {
+  return (
+    <div>
+      <h3>Seu perfil</h3>
+    </div>
+  )
+}
+
+export default Perfil
