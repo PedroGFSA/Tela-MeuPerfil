b3724de663c5897300974358cc6a1031a275904a
PedroGFSA <pedrogfsa2002@gmail.com>
2023-09-01 23:59:20 -0300

    Seu perfil

--- src/Perfil.jsx
+++ src/Perfil.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Perfil = () => {
   return (
     <div>
-      <h3>Seu perfil</h3>
+      <h3 style={{color: "#07689F",marginLeft:"1rem"}}>Seu perfil</h3>
+      <div className="containerSeuPerfil">
+        <img src="src\assets\icone-perfil.png" alt="icone de perfil" />
+        <div>
+          <h2>Carolina Magalhães</h2>
+          <p>São Paulo - SP</p>
+          <div className="estrelas">
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+            <span>(0)</span>
+          </div>
+        </div>
+      </div>
+      <h3 className="completarPerfil">Completar Perfil</h3>
     </div>
   )
 }
